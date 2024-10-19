1b4a19a6951a35bcbeb1a525db30f32bd5d39d45
MiguelSPeres <miperes@outlook.pt>
2024-10-19 16:29:54 -0300

    Changes to be committed: 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 function adicionar () {
-        // recuperar valores
+        // 1 - recuperar valores, nome do produto, quantidade e valor.
+        let produto = document.getElementById('produto').value;
+        /* O recurso split (separador), que é uma forma de separamos strings em arrays. No caso abaixo o split irá separar, 
+        cortar a string usando o '-' como ponto de corte, os membros desta separação serão colocado num array. 
+        Atribuimos à variável nomeProduto o valor deste array de índice 0. */
+        let nomeProduto = produto.split('-')[0];
+        
+        let quantidade = document.getElementById('quantidade');
+
+        // 2 -calcular o preço, o nosso subtotal.
+        // 3 - adicionar o produto no carrinho.
+        // 4 - atualiza o valor total da compra.
 }
 
 function limpar () {
